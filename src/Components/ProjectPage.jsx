f27d87a6f9ffb6c2438847751d5b358acb34085f
Kyle Chen <145894617+KyleC55@users.noreply.github.com>
2025-07-09 19:19:21 +0800

    Projects section w/ buttons

---
+++ src/Components/ProjectPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ProjectPage = () =>  {
+    return (
+        <div className="max-w-[1000px] w-full min-h-screen -mt-20 mx-auto flex flex-col md:flex-row items-center justify-center px-6 text-white gap-8">
+            <h1 className="text-5xl flex flex-col text-center text-white">
+                Project
+            </h1>
+        </div>
+    );
+};
+
+export default ProjectPage;
---
+++ src/Pages/skills.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const Skills = () => {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    );
+};
+
+export default Skills;
